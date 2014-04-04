52942fa24733975952e463a0973fb0cff05f40c6
Peter Stewart <pete.stewart@newint.org>
2014-04-04 19:18:23 +0100

    Tweaked ni_menu.js and css to got with.

--- jquery/custom/ni_menu.js
+++ jquery/custom/ni_menu.js
@@ -1,23 +1,24 @@
 (function ( $ ) {
-    var ni_tablist = new Array();
+    var ni_menulist = new Array();
     $.fn.ni_menu = function( options ) {
         // This is the easiest way to have default options.
         var settings = $.extend({
             // These are the defaults.
-            data: "ni_tab-tog",
+            css_out: "ni_menu-out",
+            duration: 300,
+            easing: "easeOutSine",
         }, options );
 
         return this.each(function(i) {
-            ni_tablist[i] = $('*[data-ni_menu]', this);
-            ni_tablist[i].each(function() {
+            ni_menulist[i] = $('*[data-ni_menu]', this);
+            ni_menulist[i].each(function() {
                 var $t = $(this);
                 var t = $t.data('ni_target');
-               // dbug(fx === "slide");
-                       $(t).addClass('ni_menu-out');
-                        //$(t).css(m[1], '110%');
-                        $t.on('click', ni_menuSlide);
-                   //$(this).on('click', ni_tabClick);
-                //$(this).data('ni_tab-num',i);
+                var css_out = $t.attr('data-ni_css-out') ? $t.attr('data-ni_css-out') : settings.css_out;
+                $(t).addClass(css_out);
+                $t.on('click', ni_menuSlide);
+                $t.data('ni_i-num',i);
+                $t.data('ni_settings',settings);
             });
             // Do something to each element here.
         });
@@ -26,22 +27,20 @@
     function ni_menuSlide(e) {
         var $t = $(e.target);
         var t = $t.data('ni_target');
-        var d = $t.data('ni_menu').split(',');
-        var $m = $(t);
-        /*var c = d[1];
-        var p = $m.css(c);
-        var s = $m.data('ni_start');
-        dbug("p: "+p+" s: "+s);
-        if(p === s) {
-            var j = c === 'left' ? {left : "110%"} : {right : "110%"};
-            $m.animate(j);
-        }
-        else {
-            dbug(c+' : '+s);
-            var j = c === 'left' ? {left : s} : {right : s};
-            $m.animate(j);
-        }*/
-        $m.toggleClass('ni_menu-out', 300, "easeOutSine" );
+        var s = $t.data('ni_settings');
+        var i = $t.data('ni_i-num');
+        ni_menulist[i].each(function(){
+            var $tt = $(this);
+            var tt = $tt.data('ni_target');
+            var css_out = $tt.attr('data-ni_css-out') ? $tt.attr('data-ni_css-out') : s.css_out;
+            var d = $tt.attr('data-ni_duration') ? parseInt($tt.attr('data-ni_duration')) : s.duration;
+            var easing = $tt.attr('data-ni_easing') ? $tt.attr('data-ni_easing') : s.easing;
+            if(t != tt) {
+                if(!$(tt).hasClass(css_out)) $(tt).addClass(css_out, d, easing );
+            } else {
+                $(tt).toggleClass(css_out, d, easing );
+            }
+        });  
         return false;   
     }
 }( jQuery ));
